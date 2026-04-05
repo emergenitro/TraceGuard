5954cfda1aae49dfe1a979d1b9a7352c15d08a2b
Karthik Sankar <emergenitro@gmail.com>
2026-04-05 15:29:02 +0800

    fixed the stupid font issue

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
         />
         <link
           rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=block"
         />
       </head>
       <body className="bg-background text-on-surface antialiased">
